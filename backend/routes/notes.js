76e0db8531c7779a7dcd867c57a60fac09ccba01
Umar-Farooq-217 <uf29664@gmail.com>
2026-09-18 17:44:42 +0500

    fetchallnotes

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const fetchuser = require('../middleware/fetchuser')
+const Notes = require('../models/Notes')
+router.get('/fetchallnotes',fetchuser , async (req , res )=>{
+    // Route:1 Fetch all notes using GET "/api/notes/fetchallnotes"
+    const notes = await Notes.find({ user : req.user.id});
+    res.json(notes);
 
-router.get('/', (req , res )=>{
-    
-    res.json([]);
+
+
+
+   
 })
 module.exports = router;
