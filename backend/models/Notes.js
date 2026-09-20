ee9962e83dbde53c7c5bc9762fd3f3c5e4c0a07d
Umar-Farooq-217 <uf29664@gmail.com>
2026-09-20 07:06:12 +0500

    addnote

--- backend/models/Notes.js
+++ backend/models/Notes.js
@@ -1,22 +1,34 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
+
 const NotesSchema = new Schema({
-    title : {
-        type : String,
-        required : true
+
+    // Each note belongs to a particular user
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user'
     },
-   
-    description  : {
-        type : String , 
-        required : true ,
+
+    title: {
+        type: String,
+        required: true
     },
-    tag : {
-        type : String,
-        default : "General",
+
+    description: {
+        type: String,
+        required: true
     },
-    date : {
-        type : Date,
-        default : Date.now,
+
+    tag: {
+        type: String,
+        default: "General"
+    },
+
+    date: {
+        type: Date,
+        default: Date.now
     }
-})
-module.exports = mongoose.model('notes' , NotesSchema);
+
+});
+
+module.exports = mongoose.model('notes', NotesSchema);
--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -2,14 +2,35 @@ const express = require('express');
 const router = express.Router();
 const fetchuser = require('../middleware/fetchuser')
 const Notes = require('../models/Notes')
+
+// Route:1 Fetch all notes using GET "/api/notes/fetchallnotes" , Login required
 router.get('/fetchallnotes',fetchuser , async (req , res )=>{
-    // Route:1 Fetch all notes using GET "/api/notes/fetchallnotes"
-    const notes = await Notes.find({ user : req.user.id});
+    try {
+         const notes = await Notes.find({ user : req.user.id});
     res.json(notes);
-
-
-
-
+    } catch (error) {
+        return res.status(500).send("Internal Server Error")
+    }
+     
+})
+// Route:2 post new notes using "/api/notes/addnote" , Login required
+router.post('/addnote' ,fetchuser, async(req ,res )=>{
+    try {
+       const {title , description , tag } = req.body;
+    if(!title && !description && !tag){
+        return res.status(500).send( "Add all fields")
+    }
+    if(!title.length > 5 && !description.length > 5 && !tag.length > 3){
+        return res.status(500).send("title length > 5 && description length > 5 && tag length > 3")
+    }
+    const note = new Notes({
+        title , description , tag , user : req.user.id
+    })
+    const savedNote = await note.save();
+    res.json(savedNote)  
+    } catch (error) {
+        return res.status(500).send("Internal Server Error")
+    }
    
 })
 module.exports = router;
